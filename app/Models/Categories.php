093b5a65afdde60ae996f12a3afcf26034f0f670
riyannurhidayat01 <riyannurhidayat298@gmail.com>
2025-04-22 09:59:26 +0700

    Database - Menambahkan tabel Products

--- app/Models/Categories.php
+++ app/Models/Categories.php
@@ -1,9 +1,10 @@
 <?php
+
 namespace App\Models;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Database\Eloquent\Model;
-class ProductCategory extends Model
+
+class Categories extends Model
 {
- use HasFactory;
- protected $table = 'product_categories'; // Sesuaikan dengannama tabel kalian
+    //
 }
